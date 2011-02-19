5fd81c53d62f064c9362c03adadf8f525d12111a
Mikael Hallendal <mhallendal@gmail.com>
2011-02-19 20:09:22 +0100

    Renamed BubbleView to ImageBubbleView.

---
+++ BubbleView/ImageBubbleView.h
@@ -0,0 +1,16 @@
+//
+//  BubbleView.h
+//  BubbleView
+//
+//  Created by Mikael Hallendal on 2011-02-18.
+//  Copyright 2011 Mikael Hallendal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ImageBubbleView : UIView {
+    
+}
+
+@end
